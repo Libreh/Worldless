c7baff8c70f1b5f687ede17b3773fc2811743d8c
libreh <beverlydlashley@protonmail.com>
2026-02-24 19:22:46 +0100

    Optimize imports

--- src/main/java/me/libreh/worldreset/command/WorldResetCommand.java
+++ src/main/java/me/libreh/worldreset/command/WorldResetCommand.java
@@ -3,7 +3,6 @@ package me.libreh.worldreset.command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.LongArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
